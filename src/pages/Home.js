74b0dcf12359b641fc789957efae751d4a68375a
Jackson <jackson@jacksonmoody.com>
2023-01-02 01:30:59 -0700

    Formatting changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
+import Example from '../components/Example';
+
 function Home() {
     return (
         <div className="Home">
             <h1>Home</h1>
+            <h3>Let's add our first components here</h3>
+            <Example />
         </div>
     );
 }
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -3,13 +3,14 @@ import Box from '@mui/material/Box';
 import { Button } from '@mui/material';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import {auth} from "../helpers/firebase";
+import { auth } from "../helpers/firebase";
 import { Outlet, useNavigate } from 'react-router-dom';
+import '../styling/Layout.css'
 
 function Layout(props) {
 
   const navigate = useNavigate();
-  
+
   function logout() {
     auth.signOut();
     navigate('/login');
@@ -21,13 +22,20 @@ function Layout(props) {
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              T4SG Wintersession Project | Welcome{props.username ? ", " + props.username : "!"}
+              T4SG Wintersession Project | Welcome{props.username ? ", " + props.username + "!" : "!"}
             </Typography>
-            <Button color = "inherit" onClick = {logout}>Logout</Button>
+            <Box sx={{ pr: 2, display: { xs: 'none', md: 'flex'} }}>
+                <Button color = "inherit" onClick = {() => navigate('/')}>Home</Button>
+                <Button color = "inherit" onClick = {() => navigate('/profile')}>Profile</Button>
+                <Button color = "inherit" onClick = {() => navigate('/results')}>Results</Button>
+            </Box>
+            <Button variant = "outlined" color = "inherit" onClick={logout}>Logout</Button>
           </Toolbar>
         </AppBar>
       </Box>
-      <Outlet />
+      <div className = "outlet">
+        <Outlet />
+      </div>
     </>
   );
 }
