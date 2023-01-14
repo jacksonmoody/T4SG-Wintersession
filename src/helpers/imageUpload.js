d584c240147666817e8990adc9b549ee0102860a
Jackson <jackson@jacksonmoody.com>
2023-01-14 15:56:48 -0700

    Troubleshooting environment variables

--- src/helpers/imageUpload.js
+++ src/helpers/imageUpload.js
@@ -3,8 +3,8 @@ function uploadImageCallBack(file) {
       (resolve, reject) => {
         const xhr = new XMLHttpRequest(); 
         xhr.open('POST', 'https://api.imgur.com/3/image');
-        xhr.setRequestHeader('Authorization', 'Client-ID ' + process.env.clientId);
-        console.log(process.env.clientId);
+        xhr.setRequestHeader('Authorization', 'Client-ID ' + process.env.REACT_APP_CLIENT_ID);
+        console.log(process.env.REACT_APP_CLIENT_ID);
         const data = new FormData(); 
         data.append('image', file);
         xhr.send(data);
