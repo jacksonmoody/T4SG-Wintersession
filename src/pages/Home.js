ec842675b41fd83e25852a0a5f80eff0daee7786
nikki2906 <nhihuynh2906@gmail.com>
2023-01-16 18:20:27 -0500

    added view other profiles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import HowBlockingWorks from '../components/HowBlockingWorks';
 import Title from '../components/Title';
-
+import OtherProfiles from '../src/components/OtherProfiles';
 
 function Home() {
     return (
         <div className="Home">
             <Title />
             <HowBlockingWorks />
+            <OtherProfiles />
         </div>
     );
 }
