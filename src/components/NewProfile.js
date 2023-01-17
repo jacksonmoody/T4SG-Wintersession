33ad06ee68fdd3e6be4a180bc4acbec5d19b4b91
Tsiyon Geremew <tsiyongeremew@college.harvard.edu>
2023-01-17 13:24:09 -0500

    how to create a profile

---
+++ src/components/NewProfile.js
@@ -0,0 +1,16 @@
+function CreateProfile(props) {
+    //Anything passed into the component can be accessed via props.(something)
+    return (
+        <div>
+            <h1>How to create a Profile</h1>
+            <p>Welcome! To begin the process, you must first sign-up account. 
+                <br /> 1. Fill out the questionnaire 
+                <br /> 2. Click the top right corner of the screen on the user icon. 
+                <br /> 3. Edit your information such as including a profile picture. 
+                <br /> 4. Once you have finished filling out your information, Scroll down to the bottom of the screen. </p>
+
+        </div>
+    );
+}
+
+export default CreateProfile;
