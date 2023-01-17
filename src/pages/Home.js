775708e8a73d768c07c76dcbad32b89aeb14df0c
Jackson <jackson@jacksonmoody.com>
2023-01-16 17:21:16 -0700

    Deleted example

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,9 @@
-import Example from '../components/Example';
 import HowBlockingWorks from '../components/HowBlockingWorks';
 
 function Home() {
     return (
         <div className="Home">
             <h1>Home</h1>
-            <h3>Let's add our first components here</h3>
-            <Example />
             <HowBlockingWorks />
         </div>
     );
