1d8ffae3fa99c7f65a0efbbfeb88c3662b563967
Brianna Chan <briannachan3@gmail.com>
2023-01-16 23:46:29 -0500

    Title component edit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
-import Example from '../components/Example';
+import Title from '../components/Title';
 
 function Home() {
     return (
         <div className="Home">
             <h1>Home</h1>
-            <h3>Let's add our first components here</h3>
-            <Example />
+            <Title />
         </div>
     );
 }
