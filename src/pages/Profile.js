c338c0c30733ec82f589976b0f4eb9de3816aaf4
cfharvard <112586108+cfharvard@users.noreply.github.com>
2023-01-17 18:31:33 -0500

    Some profile stuff

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,7 @@
 import ResultsTable from '../components/ResultsTable';
 import React from 'react'
-
+import "../styling/Profile.css"
+/*
 function Profile(props) {
     const headers = ["Name", "Email", "Description"];
     let body = props.users.filter((user) => (user.uid === props.currentUser.uid));
@@ -12,6 +13,20 @@ function Profile(props) {
             <ResultsTable header = {headers} body = {body}/>
         </div>
     );
-}
+} */
 
+
+function Profile(props) {
+    const currentUser = props.users.filter((user) => (user.uid === props.currentUser.uid))[0];
+    
+    return ( <div className="Profile">
+                <div className="userinfo">
+                    <img alt="Profile" id="profilePicture" src = { currentUser.image }></img>
+                    <h1> { currentUser.name }</h1>
+                    <h2> { currentUser.dorm } </h2> 
+                </div>
+                
+            </div>)
+
+}
 export default Profile;
