02fd2f7c5e855e3e0aead732dd5140ba498a2bce
nikki2906 <nhihuynh2906@gmail.com>
2023-01-17 01:15:15 -0500

    imported react to different pages, modified otherprofiles.js and added otherprofiles.css.

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-
+import React from 'react'
 function ProtectedRoute(props) {
     if (!props.loggedIn) {
         return <Navigate to="/login" replace/>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import OtherProfiles from '../components/OtherProfiles';
+import React from 'react'
 
 function Home() {
     return (
         <div className="Home">
             <h1>Home</h1>
+            <h3>How To View Other Profile</h3>
             <OtherProfiles />
         </div>
     );
