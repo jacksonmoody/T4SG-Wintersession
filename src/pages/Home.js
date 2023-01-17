453dd4495302d762b39aae6e84663d42a04b8177
Jackson <jackson@jacksonmoody.com>
2023-01-16 17:20:33 -0700

    Fixed routing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import Example from '../components/Example';
-import OtherProfiles from '../src/components/OtherProfiles';
+import OtherProfiles from '../components/OtherProfiles';
 
 function Home() {
     return (
         <div className="Home">
             <h1>Home</h1>
-            <h3>Let's add our first components here</h3>
-            <Example />
             <OtherProfiles />
         </div>
     );
