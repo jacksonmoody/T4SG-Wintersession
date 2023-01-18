ff849639da2fd663912e0e19aa7e6576cdeaf676
Jackson <jackson@jacksonmoody.com>
2023-01-18 15:24:04 -0700

    Fixed component nesting

--- src/components/NewProfile.js
+++ src/components/NewProfile.js
@@ -3,14 +3,14 @@ function CreateProfile(props) {
     return (
         <div>
             <h1>How To Create a Profile</h1>
-            <body>Welcome! To begin the process, you must first sign-up for an account. 
+            <div>Welcome! To begin the process, you must first sign-up for an account. 
                <ol>
                     <li>Fill out the questionnaire </li>
                     <li>Click the top right corner of the screen on the user icon. </li>
                     <li>Edit your information such as including a profile picture. </li>
                     <li>Once you have finished filling out your information, scroll down to the bottom of the screen. </li>
                 </ol>
-            </body>
+            </div>
 
         </div>
     );
