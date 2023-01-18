aeb10d81fc8b1f27de91b4ce2c189a61d85b61b5
Jackson <jackson@jacksonmoody.com>
2023-01-17 20:01:29 -0700

    Added arrayToString

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,12 +1,13 @@
 import ResultsTable from '../components/ResultsTable';
 import algorithm from '../helpers/algorithm';
-import React from 'react'
+import React from 'react';
+import arrayToString from '../helpers/arrayToString';
 
 function Results(props) {
     const headers = ["Name", "Email", "Description", "Clubs", "Gender", "Number of Blockmates", "Concentration", "Cleanliness", "Sleep Time", "Hobbies", "Room Loudness", "Looking for Group?", "Dorm", "Type of Person"];
     const currentUser = props.users.filter((user) => (user.uid === props.currentUser.uid))[0];
     const users = algorithm(currentUser.preferences, props.users);
-    const body = users.map((user) => ([user.name, user.email, user.description, user.clubs, user.gender, user.numOfBlockmates, user.concentration, user.cleanliness, user.sleeptime, user.hobbies, user.roomloudness, user.lookingforgroup, user.dorm, user.TypeofPerson]));
+    const body = users.map((user) => ([user.name, user.email, user.description, arrayToString(user.clubs), user.gender, user.numOfBlockmates, arrayToString(user.concentration), user.cleanliness, user.sleeptime, arrayToString(user.hobbies), user.roomloudness, user.lookingforgroup, user.dorm, user.TypeofPerson]));
 
     return (
         <div className="Results">
