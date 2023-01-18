16f6dad8d6ed631e9eaceec36cdb11829480d25e
coleyellin <112836508+coleyellin@users.noreply.github.com>
2023-01-17 18:29:01 -0800

    Results page with new headings

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -3,10 +3,10 @@ import algorithm from '../helpers/algorithm';
 import React from 'react'
 
 function Results(props) {
-    const headers = ["Name", "Email", "Description"];
+    const headers = ["Name", "Email", "Description", "Clubs", "Gender", "Number of Blockmates", "Concentration", "Cleanliness", "Sleep Time", "Hobbies", "Room Loudness", "Looking for Group?", "Dorm", "Type of Person"];
     const currentUser = props.users.filter((user) => (user.uid === props.currentUser.uid))[0];
     const users = algorithm(currentUser.preferences, props.users);
-    const body = users.map((user) => ([user.name, user.email, user.description]));
+    const body = users.map((user) => ([user.name, user.email, user.description, user.clubs, user.gender, user.numOfBlockmates, user.concentration, user.cleanliness, user.sleeptime, user.hobbies, user.roomloudness, user.lookingforgroup, user.dorm, user.TypeofPerson]));
 
     return (
         <div className="Results">
