b36eeb1bd1f9f3bef5cdb515633f01f1a5be996a
Jackson <jackson@jacksonmoody.com>
2023-01-18 13:26:59 -0700

    Updated Error Page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from "@mui/material/Button";
+import HomeIcon from '@mui/icons-material/Home';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 function ErrorPage() {
+    const navigate = useNavigate();
+
     return (
         <div className="Error-Page">
-            <h1>The page was not found</h1>
+            <Container maxWidth = "sm">
+                <Box textAlign = 'center' sx = {{marginTop: 8}}>
+                    <h1>Sorry, the page was not found.</h1>
+                    <Button variant="contained" startIcon={<HomeIcon />} style={{ minWidth: '200px', minHeight: '60px', fontSize: '18px', backgroundColor: "##1e90ff", marginTop: 15 }} onClick={() => navigate('/')}>Go Home</Button>
+                </Box>
+
+            </Container>
         </div>
     );
 }
