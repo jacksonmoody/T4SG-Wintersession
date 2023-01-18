b60770cc309c392ae5e4c518eea88d5bceaf02a2
Jackson <jackson@jacksonmoody.com>
2023-01-18 16:00:33 -0700

    Updated onboarding flow

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -1,131 +1,137 @@
 import { useState } from "react";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { updateUser } from "../helpers/database";
-import imageUpload from "../helpers/imageUpload";
+import Onboarding1 from "./Onboarding1";
+import Onboarding2 from "./Onboarding2";
 import { useNavigate } from "react-router-dom";
-import TextField from "@mui/material/TextField";
-import Collapse from "@mui/material/Collapse";
-import Alert from "@mui/material/Alert";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-import React from 'react';
+import imageUpload from "../helpers/imageUpload";
+import { updateUser } from "../helpers/database";
 
-function Onboarding(props) {
-
+function Onboarding({ users, currentUser }) {
+    const [step, setStep] = useState(1);
+    const [successAlert, setSuccessAlert] = useState(false);
     const [description, setDescription] = useState("");
     const [selectedImage, setSelectedImage] = useState(null);
-    const [successAlert, setSuccessAlert] = useState(false);
+    const [concentration, setConcentration] = useState("");
+    const [clubs, setClubs] = useState([]);
+    const [gender, setGender] = useState("");
+    const [currentBlockmates, setCurrentBlockMates] = useState(0);
+    const [cleanliness, setCleanliness] = useState(0);
+    const [sleepTime, setSleepTime] = useState(0);
+    const [hobbies, setHobbies] = useState([]);
+    const [roomLoudness, setRoomLoudness] = useState(0);
+    const [lookingForGroup, setLookingForGroup] = useState(false);
+    const [dorm, setDorm] = useState("");
+    const [typeOfPerson, setTypeOfPerson] = useState("");
     const navigate = useNavigate();
 
-    let users;
-    let user;
-    let name;
-
-    try {
-        users = props.users.filter((user) => (user.uid === props.currentUser.uid));
-        user = users[0];
-        name = user.name;
-    } catch(e) {
-        return null;
+    const nextStep = () => {
+        setStep(step + 1);
     }
 
-    async function finish_onboarding(e) {
-        e.preventDefault();
-        if(selectedImage) {
+    const handleChange = (e, input) => {
+        switch (input) {
+            case "description":
+                setDescription(e.target.value);
+                break;
+            case "image":
+                setSelectedImage(e.target.files[0]);
+                setSuccessAlert(true);
+                break;
+            case "concentration":
+                setConcentration(e.target.value);
+                break;
+            case "clubs":
+                setClubs(e.target.value);
+                break;
+            case "gender":
+                setGender(e.target.value);
+                break;
+            case "currentBlockmates":
+                setCurrentBlockMates(e.target.value);
+                break;
+            case "cleanliness":
+                setCleanliness(e.target.value);
+                break;
+            case "sleepTime":
+                setSleepTime(e.target.value);
+                break;
+            case "hobbies":
+                setHobbies(e.target.value);
+                break;
+            case "roomLoudness":
+                setRoomLoudness(e.target.value);
+                break;
+            case "lookingForGroup":
+                setLookingForGroup(e.target.value);
+                break;
+            case "dorm":
+                setDorm(e.target.value);
+                break;
+            case "typeOfPerson":
+                setTypeOfPerson(e.target.value);
+                break;
+            default:
+                break;
+        }
+    }
+
+    async function submit_onboarding() {
+
+        let user;
+
+        try {
+            users = users.filter((user) => (user.uid === currentUser.uid));
+            user = users[0];
+        } catch (e) {
+            return null;
+        }
+
+        if (selectedImage) {
             const result = await imageUpload(selectedImage);
             user.image = result.data.link;
         } else {
             user.image = "https://1000logos.net/wp-content/uploads/2017/02/Harvard-Logo.png";
         }
-        user.description = description;
-        user.onboarded = true;
+
+        try {
+            user.description = description;
+            user.intendedConcentrations = concentration;
+            user.clubs = clubs;
+            user.gender = gender;
+            user.numBlockmates = currentBlockmates;
+            user.cleanlinessLevel = cleanliness;
+            user.sleepTime = sleepTime;
+            user.hobbies = hobbies;
+            user.roomLoudness = roomLoudness;
+            user.lookingForGroup = lookingForGroup;
+            user.dorm = dorm;
+            user.typeOfPerson = typeOfPerson;
+            user.onboarded = true;
+        } catch (e) {
+            user = { description: "", intendedConcentrations: "", clubs: [], gender: "", numBlockmates: 0, cleanlinessLevel: 0, sleepTime: 0, hobbies: [], roomLoudness: 0, lookingForGroup: false, dorm: "", typeOfPerson: "", onboarded: true }
+        }
+        
+        console.log("Updating user...");
+        console.log(user);
+
         await updateUser(user);
         navigate('/');
     }
 
-    return (
-        <Container component="main" maxWidth="s">
-            <CssBaseline />
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}
-            >
-                <Typography component="h1" variant="h3">
-                    Welcome, {name}!
-                </Typography>
-                <Typography component="h1" variant="h5" sx={{ mt: 2 }}>
-                    Let's get started creating your profile.
-                </Typography>
-                <Box component="form" onSubmit={finish_onboarding} sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="description"
-                        label="Description"
-                        name="description"
-                        autoComplete="description"
-                        autoFocus
-                        multiline
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                    <Collapse in={successAlert}>
-                        <Alert
-                            action={
-                                <IconButton
-                                    aria-label="close"
-                                    color="inherit"
-                                    size="small"
-                                    onClick={() => {
-                                        setSuccessAlert(false);
-                                    }}
-                                >
-                                    <CloseIcon fontSize="inherit" />
-                                </IconButton>
-                            }
-                        >
-                            Image upload success!
-                        </Alert>
-                    </Collapse>
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        sx={{ mt: 3, mb: 1 }}
-                        component="label"
-                    >
-                        Upload Profile Picture
-                        <input
-                            type="file"
-                            accept="image/*"
-                            hidden
-                            onChange={(e) => {
-                                setSelectedImage(e.target.files[0]);
-                                setSuccessAlert(true);
-                                }
-                            }
-                        />
-                    </Button>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 1 }}
-                    >
-                        Submit
-                    </Button>
-                </Box>
-            </Box>
-        </Container>
-    );
+    switch (step) {
+        case 1:
+            return (
+                <Onboarding1 nextStep={nextStep} handleChange={handleChange} users={users} currentUser={currentUser} successAlert={successAlert} />
+            )
+        case 2:
+            return (
+                <Onboarding2 nextStep={nextStep} handleChange={handleChange} />
+            )
+        case 3:
+            submit_onboarding();
+            break;
+        default:
+            break;
+    }
 }
 
 export default Onboarding;
---
+++ src/pages/Onboarding1.js
@@ -0,0 +1,91 @@
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import Collapse from "@mui/material/Collapse";
+import Alert from "@mui/material/Alert";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+function Onboarding1({ users, currentUser, nextStep, handleChange, successAlert}) {
+
+    let theUsers;
+    let user;
+    let name;
+
+    try {
+        theUsers = users.filter((user) => (user.uid === currentUser.uid));
+        user = theUsers[0];
+        name = user.name;
+    } catch(e) {
+        return null;
+    }
+
+    return (
+        <Container component="main" maxWidth="s">
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Typography component="h1" variant="h3">
+                    Welcome, {name}!
+                </Typography>
+                <Typography component="h1" variant="h5" sx={{ mt: 2 }}>
+                    Let's get started creating your profile.
+                </Typography>
+                <Box sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="description"
+                        label="Introduce yourself here!"
+                        name="description"
+                        autoComplete="description"
+                        multiline={true}
+                        rows={3}
+                        onChange={(e) => handleChange(e, "description")}
+                    />
+                    <Collapse in={successAlert}>
+                        <Alert>
+                            Image upload success!
+                        </Alert>
+                    </Collapse>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        sx={{ mt: 3, mb: 1 }}
+                        component="label"
+                    >
+                        Upload Profile Picture
+                        <input
+                            type="file"
+                            accept="image/*"
+                            hidden
+                            onChange={(e) => handleChange(e, "image")}
+                        />
+                    </Button>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        endIcon={<ArrowForwardIcon />}
+                        sx={{ mt: 3, mb: 1 }}
+                        onClick={() => nextStep()}
+                    >
+                        Continue 
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
+    );
+}
+
+export default Onboarding1;
