0c82be1f247ae71a3143a1af9d6c04966c7ec8ff
Jackson <jackson@jacksonmoody.com>
2023-01-18 18:45:32 -0700

    Debugging

--- src/helpers/SubmitToDatabase.js
+++ src/helpers/SubmitToDatabase.js
@@ -3,20 +3,17 @@ import { useNavigate } from "react-router-dom";
 import imageUpload from "../helpers/imageUpload";
 import { updateUser } from "./database";
 
-export default function SubmitToDatabase({image, user}){
+export default function SubmitToDatabase({ image, user }) {
     const navigate = useNavigate();
 
     useEffect(() => {
         async function submit_data() {
             let userToUpdate = user;
-
+            userToUpdate.image = "https://1000logos.net/wp-content/uploads/2017/02/Harvard-Logo.png";
             if (image) {
                 const result = await imageUpload(image);
                 userToUpdate.image = result.data.link;
-            } else {
-                userToUpdate.image = "https://1000logos.net/wp-content/uploads/2017/02/Harvard-Logo.png";
             }
-
             await updateUser(userToUpdate);
             navigate("/");
 
@@ -24,9 +21,12 @@ export default function SubmitToDatabase({image, user}){
 
         submit_data();
 
-    },[]);
-    
-    return(
-        <p>Submitting to database. Please wait.</p>
+    }, []);
+
+    return (
+        <>
+            <h3>Submitting to database. Please wait.</h3>
+            <p>If this is taking forever, it's probably an issue with <a href="https://api.imgur.com/">Imgur</a> lol.</p>
+        </>
     )
 }
