b62a327503f6adc913b4fe45ebd3a82e9250604e
nikki2906 <nhihuynh2906@gmail.com>
2023-01-19 03:23:05 -0500

    modified quiz and onboarding according to the instruction

--- src/pages/CompatibilityQuiz1.js
+++ src/pages/CompatibilityQuiz1.js
@@ -4,11 +4,10 @@ import { Box } from "@mui/material";
 import { Button } from "@mui/material";
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 
-function CompatibilityQuiz1 () {
+function CompatibilityQuiz1 ({ nextStep, handleChange }) {
     const [value1, setValue1] = useState(0);
     const [value2, setValue2] = useState(0);
 
@@ -35,7 +34,7 @@ function CompatibilityQuiz1 () {
                 <Box style={{margin: "0.5rem", padding: "0.1rem", width: "80px", backgroundColor: "lightblue", borderRadius: "10px"}} align="center">
                     <p style={{fontSize: "12px"}}>Your Answer</p>
                 </Box>
-                <Slider
+                <Slider>
                     size="small"
                     defaultValue={5}
                     aria-label="Small"
@@ -50,13 +49,14 @@ function CompatibilityQuiz1 () {
                         {value: 10, label: 'Regularly'},
                     ]}
                     style={{ width: 400 }}
-                />
+                    onChange={(e) => handleChange(e, "cleanliness")}
+                </Slider>
                 </Grid>
                 <Grid item align="center">
                 <Box style={{margin: "0.5rem", padding: "0.1rem", width: "200px", backgroundColor: "lightblue", borderRadius: "10px"}} align="center">
                     <p style={{fontSize: "12px"}}>Preferred Blockmate Answer</p>
                 </Box>
-                <Slider
+                <Slider>
                     size="small"
                     defaultValue={5}
                     aria-label="Small"
@@ -71,15 +71,13 @@ function CompatibilityQuiz1 () {
                         {value: 10, label: 'Regularly'},
                     ]}
                     style={{ width: 400 }}
-                />
+                    onChange={(e) => handleChange(e, "cleanliness")}
+                </Slider>
                 </Grid>
                 <Grid item align="center">
-                <Link to="/" style={{textDecoration: 'none'}} onClick={handleContinue}>
-                <Button variant="contained" color="primary" style={{ textDecoration: 'none' }} endIcon={<ArrowForwardIcon />}>
+                <Button variant="contained" color="primary" style={{ textDecoration: 'none' }} endIcon={<ArrowForwardIcon />} onClick={() => nextStep()}>
                      Continue
                 </Button>
-                </Link>
-                
             </Grid>
             </div>
         </Grid>
