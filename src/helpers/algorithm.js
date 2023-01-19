a0ef5c17ec5c876cd6aedf929a4d071792fda8d7
coleyellin <112836508+coleyellin@users.noreply.github.com>
2023-01-19 14:28:11 -0500

    updated algorithm

--- src/helpers/algorithm.js
+++ src/helpers/algorithm.js
@@ -11,13 +11,36 @@
 // }
 //The algorithm will iterate through the preferences and check which users have the closest matches
 
-function algorithm(preferences, users) {
+function algorithm(currentUser, users) {
+    const preferences = ["clubs", "intendedConcentrations", "cleanlinessLevel", "sleepTime", "hobbies", "roomLoudness", "typeOfPerson"];
     users.forEach(
         (user) => {
             user.score = 0;
             for (let i = 0; i < preferences.length; i++) {
-                const difference = Math.abs(preferences[i] - user.preferences[i]);
-                user.score += difference;
+                if (Array.isArray(currentUser[preferences[i]])) {
+                    const length1 = currentUser[preferences[i]].length;
+                    const length2 = user[preferences[i]].length;
+
+                    for (let j = 0; j < length1; j++) {
+                        for (let k = 0; k < length2; k++) {
+                            if (currentUser[preferences[i][j]] == user[preferences[i][k]]) {
+                                user.score += -1;
+                            }
+                        }
+                        user.score += 1;
+                    }
+                    
+                }
+                if (typeof currentUser[preferences[i]] == "string") {
+                    if (currentUser[preferences[i]] != user[preferences[i]]) {
+                        user.score += 5;
+                    }
+                }
+
+                else {
+                    const difference = Math.abs(currentUser[preferences[i]] - user[preferences[i]]);
+                    user.score += difference;
+                }
             }
         }
     )
