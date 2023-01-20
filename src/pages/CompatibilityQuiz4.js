63e77c1e1f296f17c663fa5d9d82aa85933d5686
Jackson <jackson@jacksonmoody.com>
2023-01-20 12:33:44 -0700

    Formatting changes

--- src/pages/CompatibilityQuiz4.js
+++ src/pages/CompatibilityQuiz4.js
@@ -23,13 +23,15 @@ function CompatabilityQuiz4({ nextStep, handleChange }) {
           m: 2,
           border: '4px solid black',
           borderRadius: '20px',
-          marginTop: 8,
+          marginTop: 3,
           display: "flex",
           flexDirection: "Column",
           alignItems: "center",
         }}>
-        <Typography align="center" variant="h3"> Compatability Quiz </Typography>
-        <Typography align="center" variant="h5"> Q4: What type of person are you? </Typography>
+        <Box sx = {{marginTop: 3, marginBottom: 3}}>
+          <Typography align="center" variant="h4"  sx={{ fontWeight: 'bold'}}> Compatability Quiz </Typography>
+          <Typography align="center" variant="h6" sx={{ fontWeight: 'bold'}}> Q4: What type of person are you? </Typography>
+        </Box>
         <Box sx={{ p: 3, width: 300, border: 1 }}>
           <Slider
             aria-label="Custom marks"
@@ -42,7 +44,7 @@ function CompatabilityQuiz4({ nextStep, handleChange }) {
           />
           <Typography align="center" variant="h5"> Your Answer </Typography>
         </Box>
-        <Box sx={{ p: 3, width: 300, border: 1, marginBottom: 3}}>
+        <Box sx={{ p: 3, width: 300, border: 1, marginBottom: 3 }}>
           <Slider
             aria-label="Custom marks"
             defaultValue={5}
@@ -55,13 +57,13 @@ function CompatabilityQuiz4({ nextStep, handleChange }) {
         </Box>
         <Typography align="center" variant="h5"> Rate this question! </Typography>
         <Box>
-          <Stack direction="row" spacing={1} sx = {{marginTop: 3}}>
+          <Stack direction="row" spacing={1} sx={{ marginTop: 3 }}>
             <Button variant="outlined" align="center" >Not Important</Button>
             <Button variant="outlined" align="center" >Important</Button>
             <Button variant="outlined" align="center" >Very Important</Button>
           </Stack>
         </Box>
-        <Stack direction="row" spacing={5} sx = {{marginTop: 3, marginBottom: 3}}>
+        <Stack direction="row" spacing={5} sx={{ marginTop: 3, marginBottom: 3 }}>
           <Button onClick={() => nextStep()} variant="contained" align="center" justifyContent="center" alignItems="center" > Continue</Button>
         </Stack>
       </Box>
