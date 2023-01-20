a54a095d6a5ed9bd651bddac0d07b32c0c3551cf
Jackson <jackson@jacksonmoody.com>
2023-01-19 23:12:30 -0700

    Removed unnecessary imports

--- src/pages/AcademicInterest.js
+++ src/pages/AcademicInterest.js
@@ -1,4 +1,4 @@
-import {Container, Typography, Box, Select, MenuItem, OutlinedInput, InputLabel, FormControl, Button} from "@mui/material";
+import {Container, Typography, Box, Select, MenuItem, FormControl, Button} from "@mui/material";
 import React from "react";
 
 function AcademicInterest({nextStep, handleChange}) {
