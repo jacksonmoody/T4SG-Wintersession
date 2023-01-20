739501820dee0f82ffd527f8e1267767b20bee70
Jackson <jackson@jacksonmoody.com>
2023-01-20 00:30:30 -0700

    Removed comments

--- src/helpers/algorithm.js
+++ src/helpers/algorithm.js
@@ -1,16 +1,3 @@
-//This is a basic approach to ranking other users based on a certain user's preferences
-//The preferences are an array of integers (i.e [1, 2, 3])
-//The user is a JSON object with the following structure:
-// {
-//     uid: "123",
-//     name: "John Doe",
-//     authProvider: "google",
-//     email: "",
-//     description: "",
-//     preferences: ["1", "2", "3"]
-// }
-//The algorithm will iterate through the preferences and check which users have the closest matches
-
 function algorithm(currentUser, users) {
     const preferences = ["clubs", "intendedConcentrations", "cleanlinessLevel", "sleepTime", "hobbies", "roomLoudness", "typeOfPerson"];
     users.forEach(
